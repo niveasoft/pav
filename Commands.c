2949d04d3667294ea7d3e58b81e019770ca1efd4
niveasoft <131618937+niveasoft@users.noreply.github.com>
2023-11-07 20:16:41 +0530

    Add files via upload

---
+++ Commands.c
@@ -0,0 +1,21 @@
+ping google.com
+
+nslookup facebook.com
+
+traceroute www.google.com
+
+host google.com
+
+netstat
+
+arp
+
+ifconfig
+
+dig A google.com
+
+route
+route add -net 192.168.90.0/24 gw 10.0.0.1
+
+enthool enp0s3
+
